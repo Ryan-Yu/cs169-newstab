9a54774e6ea39d575b3cf66c3402a2126b8906c2
Ryan Yu <ryan.cao.you@gmail.com>
2015-04-28 11:55:20 +0000

    RY: Precompilation of assets for Heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,41 +11,41 @@ Category.create!('name' => 'Entertainment')
 Category.create!('name' => 'Travel')
 Category.create!('name' => 'Sports')
 
-# Users
-User.create!(first_name:"Example",
-             last_name: "User",
-             email: "exampleuser@gmail.com",
-             password: "password",
-             password_confirmation: "password")
+# # Users
+# User.create!(first_name:"Example",
+#             last_name: "User",
+#             email: "exampleuser@gmail.com",
+#             password: "password",
+#             password_confirmation: "password")
 
 
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@gmail.com"
-  password = "password"
-  User.create!(first_name: name.split(' ')[0],
-              last_name: name.split(' ')[1],
-              email: email,
-              password: password,
-              password_confirmation: password)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@gmail.com"
+#   password = "password"
+#   User.create!(first_name: name.split(' ')[0],
+#               last_name: name.split(' ')[1],
+#               email: email,
+#               password: password,
+#               password_confirmation: password)
+# end
 
-# Articles
-users = User.order(:created_at).take(6)
-50.times do
-  fake_url = Faker::Lorem.sentence(1)
-  fake_comment = Faker::Lorem.sentence(2)
-  fake_content = Faker::Lorem.sentence(4)
-  fake_title = Faker::Lorem.sentence(6)
-  users.each { |user| user.articles.create!(url: fake_url, initial_comment: fake_comment,
-    content: fake_content, categories: Category.all, title: fake_title ) }
-end
+# # Articles
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   fake_url = Faker::Lorem.sentence(1)
+#   fake_comment = Faker::Lorem.sentence(2)
+#   fake_content = Faker::Lorem.sentence(4)
+#   fake_title = Faker::Lorem.sentence(6)
+#   users.each { |user| user.articles.create!(url: fake_url, initial_comment: fake_comment,
+#     content: fake_content, categories: Category.all, title: fake_title ) }
+# end
 
-# Following relationships
-users = User.all
-user  = users.first
-subscribing = users[2..50]
-subscribers = users[3..40]
-subscribing.each { |subscribed| user.subscribe(subscribed) }
-subscribers.each { |subscriber| subscriber.subscribe(user) }
+# # Following relationships
+# users = User.all
+# user  = users.first
+# subscribing = users[2..50]
+# subscribers = users[3..40]
+# subscribing.each { |subscribed| user.subscribe(subscribed) }
+# subscribers.each { |subscriber| subscriber.subscribe(user) }
